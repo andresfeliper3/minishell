7b9dc78e220add6da84ab812ebe220373eb5da4e
andresfeñliper3 <andresfeliper.2001@gmail.com>
2021-04-18 17:22:16 +0000

    funciona con una palabra

---
+++ shell.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <assert.h>
+#include <sys/wait.h>
+int main (int argc, char* argv[]) {
+		pid_t pid;
+		int estado;
+		char* valor[];
+		printf("Escriba\n");
+		scanf("%s", valor[0] );
+		pid = fork();
+		assert(pid >= 0);
+
+		//hijo
+		if(pid == 0) {
+			printf("Soy el hijo");
+			char *cmd[1];
+			int i;
+			for(i = 0; i < 1; i++) {
+			 cmd[i] = valor[i];
+			}
+			cmd[1] = NULL;
+			execvp(cmd[0], cmd);
+			perror("Return from execvp() not expected");
+			exit(EXIT_FAILURE);		 		
+		}
+		//padre	
+		else {
+			wait(&estado);
+			if(WIFEXITED(estado)) { //valido si el hijo sale de manera natural no eliminado por señal
+				printf(":)  %d\n", WEXITSTATUS(estado));
+			}
+		}
+
+		
+}
+
