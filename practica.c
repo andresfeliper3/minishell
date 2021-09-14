154c9829fd68eb8d43aab7bc59917a4485b425aa
JuanCamilo-R <juan.randazzo@correounivalle.edu.co>
2021-09-13 21:54:30 -0500

    Agregamos para entender el codigo en los proximos 5 meses

--- practica.c
+++ practica.c
@@ -7,7 +7,7 @@
 
 
 int main(int argc, char* argv[]){
-
+	
 	char str[100];
 	printf("digite un comando: ");
 	fgets(str, 100,stdin);
@@ -19,13 +19,27 @@ int main(int argc, char* argv[]){
 	int k = 0;
 	bool estado = false;
 	
+	/*Condicion de parada \0 ya que todo string en C (en la matriz) acaba con un \0
+	str es lo que el usuario digitó (es un array)
+	args es una matriz la cual estamos guardando cada comando por linea de la matriz
+	  0 1 2 3
+	1|l s 
+	2|
+	3| 
+	k es el iterador que recorre todo la matriz char.
+	i e j iteran la matriz args
+	*/
 	while( !(str[k] == '\0') ){
 			//ls    -l
-	   if( str[k] != ' ' && str[k] != '\r' && str[k] != '\n'){
+	   if( str[k] != ' ' /*&& str[k] != '\r'*/ && str[k] != '\n'){
 		    estado = false;
 		    args[i][j] = str[k];
-	    	    j++;		 
+	    	j++;		 
+	   		printf("%c",str[k]);
 	   }else{
+		/*Objetivo del estado: Evitar que la matriz args salte de fila cuando 
+		anteriormente ya lo había hecho.
+		(Cuando hay espacios consecutivos)*/
 	   	if (estado == false && str[k] == ' '){
 			i++;
 			j = 0;
@@ -37,7 +51,8 @@ int main(int argc, char* argv[]){
 	}
 	char *cmd[i+1];
 	for(int p = 0; p < i; p++){
-		cmd[p] = strdup(args[p+1]);
+		// cmd[p] = strdup(args[p+1]);
+		cmd[p] = args[p+1];
 	}
 	
 	cmd[i] = NULL;	
